ab0eb807d8f670f2a95ca779b5ae4a7a1e8f8def
Emmanuel Velasquez <emaster5528e@gmail.com>
2026-05-25 23:32:23 -0600

    fix db.php comillas correctas

--- db.php
+++ db.php
@@ -1,15 +1,13 @@
 <?php
-$servidor = getenv('MYSQLHOST') ?: 'localhost';
-$usuario = getenv('MYSQLUSER') ?: 'root';
+$servidor   = getenv('MYSQLHOST')     ?: 'localhost';
+$usuario    = getenv('MYSQLUSER')     ?: 'root';
 $password   = getenv('MYSQLPASSWORD') ?: '';
 $base_datos = getenv('MYSQLDATABASE') ?: 'tienda';
 $puerto     = (int)(getenv('MYSQLPORT') ?: 3306);
 
-$conexion = new mysqli($servidor, usuario, $password, $base_datos, $puerto);
-
+$conexion = new mysqli($servidor, $usuario, $password, $base_datos, $puerto);
 
 if ($conexion->connect_error) {
     die('Error de conexion: ' . $conexion->connect_error);
-    }
-    $conexion->set_charset("utf8");
-
+}
+$conexion->set_charset('utf8mb4');
